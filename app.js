784104312c9cfb2e6e6fe3f1736874661604b9dc
Tim <tim.vermant@outlook.com>
2025-01-28 11:27:51 +0100

    Testing with pushing new data to json file

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
+const { Console } = require("console");
 
 
 keepConsoleOpen();
 
-function keepConsoleOpen()
-{
+function keepConsoleOpen() {
+
+    // BookInput();
+    // const currentBook = {
+    //     title: "Elantris",
+    //     Author: "Brandon Sanderson"
+    // }
+    const currenBookList = BookInput();
+
+
+
+
+    const fs = require("fs");
+
+    const JSON_FILE = "BookCollection.json";
+
+    try {
+        const jsonData = fs.readFileSync(JSON_FILE);
+
+        const currentBooks = JSON.parse(jsonData);
+        IsBookInLibrary(currentBooks);
+
+
+
+        console.log(currentBooks);
+
+        // const newBookList = Object.assign({},currentBooks,currenBookList);
+        currentBooks.forEach(element => {
+            currenBookList.push(element);
+        });
+        console.log(currenBookList);
+
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+
+function BookInput() {
     const readline = require('readline');
     const rl = readline.createInterface({
         input: process.stdin,
@@ -12,14 +51,13 @@ function keepConsoleOpen()
 
 
 
- 
+
     var libraryLimit = 50;
     var books = Array(libraryLimit);
     var currentIndex = 0;
     console.log("What books would you like to add?");
     rl.on('line', (input) => {
-        if(currentIndex >= libraryLimit || input.toLowerCase() == "exit")
-        {
+        if (currentIndex >= libraryLimit || input.toLowerCase() == "exit") {
             console.log("Exiting the program, you have added " + currentIndex + " books to your library: ");
             books.forEach(book => {
                 console.log("> " + book)
@@ -32,18 +70,21 @@ function keepConsoleOpen()
         console.log('Add another book or EXIT?');
         currentIndex++;
     });
+    
 }
 
-
-class Library
+function IsBookInLibrary(currentBooks, bookToCheck)
 {
 
 }
 
-class Book
-{
-    constructor(title, author)
-    {
+
+class Library {
+
+}
+
+class Book {
+    constructor(title, author) {
         this.title = title;
         this.author = author;
     }
